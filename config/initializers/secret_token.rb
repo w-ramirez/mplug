66cd2f97216b155995546a4552c301dfec5900ac
Waldo Ramirez <waldo.ramirez@me.com>
2014-02-02 00:34:13 +0000

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mplug::Application.config.secret_key_base = '4c82e82f226747436f8d1b50c313babf89620233691dbdc0c70ce63ca903ee34c31c6457ad8d6f924742a25fe377b2b6a89235d11f90700991f9cfc0bab0a582'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Mplug::Application.config.session_store :cookie_store, key: '_mplug_session'
